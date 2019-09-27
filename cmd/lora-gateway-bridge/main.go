e594a27fc33f3631f2068f171ddf35d51461e97c
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2019-09-27 19:59:41 +0300

    workaround for the . in env variables
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- cmd/lora-gateway-bridge/main.go
+++ cmd/lora-gateway-bridge/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/brocaar/lora-gateway-bridge/cmd/lora-gateway-bridge/cmd"
+import (
+	"os"
+	"strings"
+
+	"github.com/brocaar/lora-gateway-bridge/cmd/lora-gateway-bridge/cmd"
+)
 
 //go:generate ./doc.sh
 
 var version string // set by the compiler
 
 func main() {
+	for _, pair := range os.Environ() {
+		d := strings.SplitN(pair, "=", 2)
+		os.Setenv(strings.ReplaceAll(strings.ReplaceAll(d[0], "_", "."), "..", "_"), d[1])
+	}
+
 	cmd.Execute(version)
 }
